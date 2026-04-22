48fa8ac7d2b3056032b55dbe3800875d63fc5c5c
annddvaa <anandadava93@gmail.com>
2026-04-22 20:04:12 +0700

    feat: Dashboard Page & Entry Point
    setup Firebase, Provider, dan struktur awal aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'features/auth/presentation/providers/auth_provider.dart';
+import 'features/dashboard/presentation/providers/product_provider.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
